abb857e706b2680ee85c61dc858b58283dbe25d8
Radu Alexeev <1radualexeev1@gmail.com>
2024-07-13 15:05:22 +0300

    corrected the submission form

--- client/js/request.js
+++ client/js/request.js
@@ -1,21 +1,16 @@
-const btn = document.getElementById("btn-submit");
-const reqBody = document.getElementById("window-hidden");
-const requestForm = document.getElementById("window-block")
-const answer = `
-<p id="response-text">Cообщение было отправлено и спасибо что выбрали нас! <br>C вами свяжется наш менеджер. </p>
-<img id="response-logo" src="img/logo.svg" alt="logo">
-`
+const form = document.getElementById("user-form");
+const formContainer = document.getElementById("window-block");
+const message = document.getElementById("response-text");
+const logo = document.getElementById("response-logo");
 
-btn.addEventListener("click", function() {
-    requestForm.style.display = "none";
-    reqBody.insertAdjacentHTML("beforeend", answer);
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    formContainer.style.display = "none";
+    message.style.display = "block";
+    logo.style.display = "block";
 
     setTimeout(() => {
-        window.location.href = "index.html"
-    }, 3000)
-})
-
-console.log(requestForm)
-
-
-// const responseForm = document.getElementById("")
+        window.location.href = "index.html";
+    }, 3000);
+});
